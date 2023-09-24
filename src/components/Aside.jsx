4958347cd5c4754f3eb1ef789852e1a88f10cfc7
Fernando Bassino <ferbassino@gmail.com>
2023-09-24 19:00:49 -0300

    adding general and aside css

--- src/components/Aside.jsx
+++ src/components/Aside.jsx
@@ -1,42 +1,42 @@
-import React, { useEffect, useState } from "react";
-import { useNav } from "../navZustand";
+import React from "react";
+import "./Aside.css";
 import { Link } from "react-router-dom";
 import { useTranslation } from "react-i18next";
+
 const Aside = (props) => {
   const [t, i18n] = useTranslation("global");
   const { userName, roles } = props;
 
   return (
-    <div className="vh-100  ">
-      <table className="table table-hover">
+    <div className="vh-100">
+      <table className="table">
         <thead>
           <tr>
             <th scope="col">{t("aside.general")}</th>
           </tr>
         </thead>
       </table>
-      <ul>
-        <li className="nav-item">
-          <Link to="/introduction" className="nav-link">
-            {t("aside.introduction")}
-          </Link>
-        </li>
-        <li className="nav-item">
-          <Link to="/deviceuse" className="nav-link">
-            {t("aside.deviceUse")}
-          </Link>
-        </li>
-        <li className="nav-item">
-          <Link to="/statistics" className="nav-link">
-            {t("aside.statistics")}
-          </Link>
-        </li>
-        <li className="nav-item">
-          <Link to="/documentation" className="nav-link">
-            {t("aside.documentation")}
-          </Link>
-        </li>
-      </ul>
+
+      <li>
+        <Link to="/introduction" className="nav-link">
+          {t("aside.introduction")}
+        </Link>
+      </li>
+      <li>
+        <Link to="/deviceuse" className="nav-link">
+          {t("aside.deviceUse")}
+        </Link>
+      </li>
+      <li>
+        <Link to="/statistics" className="nav-link">
+          {t("aside.statistics")}
+        </Link>
+      </li>
+      <li>
+        <Link to="/documentation" className="nav-link">
+          {t("aside.documentation")}
+        </Link>
+      </li>
 
       {roles === "reader" ? (
         <>
@@ -49,28 +49,27 @@ const Aside = (props) => {
               </tr>
             </thead>
           </table>
-          <ul>
-            <li className="nav-item">
-              <Link to="/reader" className="nav-link">
-                {t("aside.home")}
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link to="/reader/clients" className="nav-link">
-                {t("aside.clients")}
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link to="/reader/motions" className="nav-link">
-                {t("aside.tests")}
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link to="/reader/statistics" className="nav-link">
-                {t("aside.userStatistics")}
-              </Link>
-            </li>
-          </ul>
+
+          <li className="nav-item">
+            <Link to="/reader" className="nav-link">
+              {t("aside.home")}
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/reader/clients" className="nav-link">
+              {t("aside.clients")}
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/reader/motions" className="nav-link">
+              {t("aside.tests")}
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/reader/statistics" className="nav-link">
+              {t("aside.userStatistics")}
+            </Link>
+          </li>
         </>
       ) : null}
       {roles === "admin" ? (
@@ -84,34 +83,33 @@ const Aside = (props) => {
               </tr>
             </thead>
           </table>
-          <ul>
-            <li className="nav-item">
-              <Link to="/admin" className="nav-link">
-                {t("aside.home")}
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link to="/admin/users" className="nav-link">
-                Users
-              </Link>
-            </li>
-            <li>
-              <Link to="/admin/clients" className="nav-link">
-                {t("aside.clients")}
-              </Link>
-            </li>
 
-            <li className="nav-item">
-              <Link to="/admin/motions" className="nav-link">
-                {t("aside.tests")}
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link to="/admin/statistics" className="nav-link">
-                {t("aside.userStatistics")}
-              </Link>
-            </li>
-          </ul>
+          <li className="nav-item">
+            <Link to="/admin" className="nav-link">
+              {t("aside.home")}
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/admin/users" className="nav-link">
+              Users
+            </Link>
+          </li>
+          <li>
+            <Link to="/admin/clients" className="nav-link">
+              {t("aside.clients")}
+            </Link>
+          </li>
+
+          <li className="nav-item">
+            <Link to="/admin/motions" className="nav-link">
+              {t("aside.tests")}
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/admin/statistics" className="nav-link">
+              {t("aside.userStatistics")}
+            </Link>
+          </li>
         </>
       ) : null}
       {roles === "editor" ? (
@@ -125,30 +123,29 @@ const Aside = (props) => {
               </tr>
             </thead>
           </table>
-          <ul>
-            <li className="nav-item">
-              <Link to="/editor" className="nav-link">
-                {t("aside.home")}
-              </Link>
-            </li>
 
-            <li>
-              <Link to="/editor/clients" className="nav-link">
-                {t("aside.clients")}
-              </Link>
-            </li>
+          <li className="nav-item">
+            <Link to="/editor" className="nav-link">
+              {t("aside.home")}
+            </Link>
+          </li>
 
-            <li className="nav-item">
-              <Link to="/editor/motions" className="nav-link">
-                {t("aside.tests")}
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link to="/editor/statistics" className="nav-link">
-                {t("aside.userStatistics")}
-              </Link>
-            </li>
-          </ul>
+          <li>
+            <Link to="/editor/clients" className="nav-link">
+              {t("aside.clients")}
+            </Link>
+          </li>
+
+          <li className="nav-item">
+            <Link to="/editor/motions" className="nav-link">
+              {t("aside.tests")}
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/editor/statistics" className="nav-link">
+              {t("aside.userStatistics")}
+            </Link>
+          </li>
         </>
       ) : null}
 
@@ -159,13 +156,12 @@ const Aside = (props) => {
           </tr>
         </thead>
       </table>
-      <ul>
-        <li className="nav-item">
-          <Link to="/jornadas" className="nav-link">
-            {t("aside.jornadas")}
-          </Link>
-        </li>
-      </ul>
+
+      <li className="nav-item">
+        <Link to="/jornadas" className="nav-link">
+          {t("aside.jornadas")}
+        </Link>
+      </li>
 
       {/* <tr>
             <td>opcion 2</td>
--- src/views/general/Introduction.jsx
+++ src/views/general/Introduction.jsx
@@ -1,31 +1,37 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
+import "./GeneralStyle.css";
 export default function Introduction() {
   const [t, i18n] = useTranslation("global");
 
   return (
-    <div>
-      <h1>{t("introduction.title")}</h1>
-      <h2>{t("introduction.subTitle")}</h2>
+    <div className="general-body">
+      <h1 className="header">{t("introduction.title")}</h1>
+
+      <h3>{t("introduction.subTitle")}</h3>
       <h3>{t("introduction.mainOverview")}</h3>
       <h4>{t("introduction.mainOverviewP1")}</h4>
-      <h2>{t("introduction.subTitle2")}</h2>
-      <h3>{t("introduction.deviceUseTitle")}</h3>
-      <h4>{t("introduction.deviceUseP1")}</h4>
-      <h3>{t("introduction.statisticsTitle")}</h3>
-      <h4>{t("introduction.statisticsP1")}</h4>
-      <h3>{t("introduction.documentationTitle")}</h3>
-      <h4>{t("introduction.documentationP1")}</h4>
-      <h2>{t("introduction.mainOverview2")}</h2>
-      <h4>{t("introduction.mainOverview2P1")}</h4>
-      <h3>{t("introduction.userHomeTitle")}</h3>
-      <h4>{t("introduction.userHomeP1")}</h4>
-      <h3>{t("introduction.clientTitle")}</h3>
-      <h4>{t("introduction.clientP1")}</h4>
-      <h3>{t("introduction.testsTitle")}</h3>
-      <h4>{t("introduction.testsP1")}</h4>
-      <h3>{t("introduction.userStatisticsTitle")}</h3>
-      <h4>{t("introduction.userStatisticsP1")}</h4>
+      <section>
+        <h2 className="section-header">{t("introduction.subTitle2")}</h2>
+        <h3>{t("introduction.deviceUseTitle")}</h3>
+        <h4>{t("introduction.deviceUseP1")}</h4>
+        <h3>{t("introduction.statisticsTitle")}</h3>
+        <h4>{t("introduction.statisticsP1")}</h4>
+        <h3>{t("introduction.documentationTitle")}</h3>
+        <h4>{t("introduction.documentationP1")}</h4>
+      </section>
+      <section>
+        <h2 className="section-header">{t("introduction.mainOverview2")}</h2>
+        <h4>{t("introduction.mainOverview2P1")}</h4>
+        <h3>{t("introduction.userHomeTitle")}</h3>
+        <h4>{t("introduction.userHomeP1")}</h4>
+        <h3>{t("introduction.clientTitle")}</h3>
+        <h4>{t("introduction.clientP1")}</h4>
+        <h3>{t("introduction.testsTitle")}</h3>
+        <h4>{t("introduction.testsP1")}</h4>
+        <h3>{t("introduction.userStatisticsTitle")}</h3>
+        <h4>{t("introduction.userStatisticsP1")}</h4>
+      </section>
     </div>
   );
 }
