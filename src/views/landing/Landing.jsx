70a7b39265f29b33b9ff2ac4e52fdb691aa517a1
Fernando Bassino <ferbassino@gmail.com>
2024-02-27 14:06:16 -0300

    adding kinapp 2

--- src/views/landing/Landing.jsx
+++ src/views/landing/Landing.jsx
@@ -15,7 +15,7 @@ import capture3 from "./assets/capture_3.jpeg";
 import { useTranslation } from "react-i18next";
 import SelectLanguage from "../../components/SelectLanguage";
 
-const apkUrl = "http://localhost:3000/kinapp.apk";
+const apkUrl = "https://kinapp-web.vercel.app/kinapp.apk";
 function Landing() {
   const [t, i18n] = useTranslation("global");
 
